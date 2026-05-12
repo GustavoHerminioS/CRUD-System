27a7ac610d673970fde58cb4d3e3009f16d7aaa1
guguh <guguh@DESKTOP-3M56EEC>
2026-05-11 23:00:04 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem Vindo ao CRUD\n");
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
